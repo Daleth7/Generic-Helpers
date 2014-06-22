7e4c3571d71169fbd85e4ef496810c0bc9a4fd74
Daleth7 <i.publish.docs@gmail.com>
2014-06-22 14:28:29 -0700

    Initial Upload
    Uploading the first of the generic helpers

---
+++ Generic_Helpers.h.h
@@ -0,0 +1,18 @@
+#ifndef ARGUMENT_PACK_TYPE_HELPER_HHHH_HHHHHHHH___
+#define ARGUMENT_PACK_TYPE_HELPER_HHHH_HHHHHHHH___
+
+namespace Generic_Helper{
+    template <typename... TArgs>
+    struct extra_arg_pack{
+        template<template <typename...> class Container, typename T>
+            using append_to = Container<T, TArgs...>;
+    };
+
+    template <template <typename...> class... TTArgs>
+    struct extra_targ_pack{
+        template <template <typename...> class Container, typename T>
+            using append_to = Container<T, TTArgs<T>...>;
+    };
+}
+
+#endif
